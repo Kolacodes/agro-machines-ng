bfdc7657b72aad7582a05365472bb131454eca10
Bello Kolawole Mustapha <50749200+Kolacodes@users.noreply.github.com>
2020-12-31 13:03:39 +0100

    make better

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require ("express"),
+    port = process.env.PORT || 5000,
+    nodemailer = require("nodemailer"),
+    methodOverride = require("method-override"),
+    cookieParser        = require("cookie-parser"),
+    flash      = require("connect-flash"),
+    bodyParser = require("body-parser");
+
+app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+    
+    res.render("index");
+});
+
+
+
+app.listen(port, function(){
+    console.log("server just started")
+});
